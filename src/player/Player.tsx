c4ad78e264e802966c5ca8b582b8a2c4d4ebb3a3
Richard Bull <bull_richard@bahl.com>
2019-06-11 23:41:41 -0700

    Large refactor of video player. Pop up made into its own component. Video progress bar added.

--- src/player/Player.tsx
+++ src/player/Player.tsx
@@ -4,6 +4,66 @@ import { StyleSheet, View, Text, Animated } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import VideoPlayer from "react-native-video";
 import { FirestickKeys } from "../components";
+import { number } from "prop-types";
+
+interface VideoProps {
+  duration: number;
+  currentTime: number;
+}
+const VideoProgress = (props: VideoProps) => {
+  const { duration, currentTime } = props;
+  const widthPercentage = (currentTime / duration) * 100;
+  console.log(widthPercentage);
+  return (
+    <View style={{ ...styles2.videoProgress, width: `${widthPercentage}%` }} />
+  );
+};
+
+const styles2 = StyleSheet.create({
+  videoProgress: {
+    backgroundColor: "rgba(255,0,0,0.5)",
+    height: 4,
+    alignSelf: "flex-start"
+  }
+});
+
+interface VideoPopupProps {
+  icon: string;
+  text: string;
+  duration: number;
+  currentTime: number;
+  opacity: number | Animated.Value;
+}
+
+class VideoPopup extends Component<VideoPopupProps> {
+  render() {
+    const {
+      icon,
+      text,
+      duration = 0,
+      currentTime = 0,
+      opacity = 0
+    } = this.props;
+    return (
+      <Animated.View
+        style={{
+          ...styles.statusContainer,
+          opacity
+        }}
+      >
+        {currentTime && duration ? (
+          <VideoProgress duration={duration} currentTime={currentTime} />
+        ) : (
+          <View />
+        )}
+        <View style={styles.statusTextContainer}>
+          {icon ? <Icon name={icon} style={styles.statusIcon} /> : <View />}
+          {text ? <Text style={styles.statusText}>{text}</Text> : <View />}
+        </View>
+      </Animated.View>
+    );
+  }
+}
 
 interface Props {
   navigation: any;
@@ -13,13 +73,15 @@ interface State {
   fastForward: number;
   fastReverse: number;
   video: {
+    duration: number;
     progress: {
       currentTime: number;
     };
   };
-  status: {
-    display: object;
-    anim: object;
+  popup: {
+    icon: string;
+    text: string;
+    anim: Animated.Value;
   };
 }
 export default class Player extends Component<Props, State> {
@@ -27,61 +89,53 @@ export default class Player extends Component<Props, State> {
     paused: false,
     fastForward: 0,
     fastReverse: 0,
-    status: {
-      display: undefined,
-      anim: new Animated.Value(0)
-    },
     video: {
+      duration: undefined,
       progress: {
         currentTime: undefined
       }
+    },
+    popup: {
+      autoDismiss: true,
+      icon: undefined,
+      text: undefined,
+      anim: new Animated.Value(0)
     }
   };
 
   player = undefined;
 
-  dismissPopup = () =>
-  Animated.timing(
-    // Animate over time
-    this.state.status.anim, // The animated value to drive
-    {
-      toValue: 0, // Animate to opacity: 1 (opaque)
-      duration: 1000 // Make it take a while
-    }
-  ).start();
-
-  displayPopup = ({ icon, text, autoDismiss = true }) => {
+  displayPopup({ icon, text, autoDismiss = true }) {
     Animated.timing(
       // Animate over time
-      this.state.status.anim, // The animated value to drive
+      this.state.popup.anim, // The animated value to drive
       {
         toValue: 1, // Animate to opacity: 1 (opaque)
         duration: 250 // Make it take a while
       }
     ).start(); // Starts the animation
+    this.setState({
+      popup: {
+        ...this.state.popup,
+        icon,
+        text
+      }
+    });
     if (autoDismiss) {
       setTimeout(
-        this.dismissPopup,
+        () =>
+          Animated.timing(
+            // Animate over time
+            this.state.popup.anim, // The animated value to drive
+            {
+              toValue: 0, // Animate to opacity: 1 (opaque)
+              duration: 1000 // Make it take a while
+            }
+          ).start(),
         2000
       );
     }
-    this.setState({
-      status: {
-        ...this.state.status,
-        display: (
-          <Animated.View
-            style={{
-              ...styles.statusContainer,
-              opacity: this.state.status.anim
-            }}
-          >
-            <Icon name={icon} style={styles.statusIcon} />
-            <Text style={styles.statusText}>{text}</Text>
-          </Animated.View>
-        )
-      }
-    });
-  };
+  }
 
   render() {
     const { navigation } = this.props;
@@ -89,6 +143,11 @@ export default class Player extends Component<Props, State> {
       "uri",
       "http://www.html5videoplayer.net/videos/toystory.mp4"
     );
+
+    const currentTime =
+      this.state.video.progress.currentTime +
+      this.state.fastForward +
+      this.state.fastReverse;
 
     return (
       <View style={styles.container} hasTVPreferredFocus={true}>
@@ -98,12 +157,23 @@ export default class Player extends Component<Props, State> {
           }} // Can be a URL or a local file.
           style={styles.backgroundVideo}
           paused={this.state.paused}
-          onProgress={(progress: any) => this.setState({ video: { progress } })}
+          onLoad={({ duration }) =>
+            this.setState({ video: { ...this.state.video, duration } })
+          }
+          onProgress={(progress: any) =>
+            this.setState({ video: { ...this.state.video, progress } })
+          }
           ref={ref => {
             this.player = ref;
           }}
         />
-        {this.state.status.display || <View />}
+        <VideoPopup
+          icon={this.state.popup.icon}
+          text={this.state.popup.text}
+          duration={this.state.video.duration}
+          currentTime={currentTime}
+          opacity={this.state.popup.anim}
+        />
 
         <FirestickKeys
           keyPressTimeOut={500}
@@ -119,12 +189,16 @@ export default class Player extends Component<Props, State> {
           }}
           onLeftHold={() => {
             const reverseAmt = this.state.fastReverse
-            ? this.state.fastReverse - 10
-            : -10;
+              ? this.state.fastReverse - 10
+              : -10;
             this.setState({
               fastReverse: reverseAmt
             });
-            this.displayPopup({ icon: "backward", text: `${Math.abs(reverseAmt)} sec`, autoDismiss: false });
+            this.displayPopup({
+              icon: "backward",
+              text: `${Math.abs(reverseAmt)} sec`,
+              autoDismiss: false
+            });
           }}
           onLeft={() => {
             const reverseAmt = this.state.fastReverse
@@ -135,16 +209,23 @@ export default class Player extends Component<Props, State> {
                 reverseAmt + this.state.video.progress.currentTime
               );
             this.setState({ fastReverse: 0 });
-            this.displayPopup({ icon: "backward", text: `${Math.abs(reverseAmt)} sec` });
+            this.displayPopup({
+              icon: "backward",
+              text: `${Math.abs(reverseAmt)} sec`
+            });
           }}
           onRightHold={() => {
             const forwardAmt = this.state.fastForward
-            ? this.state.fastForward + 10
-            : 10;
+              ? this.state.fastForward + 10
+              : 10;
             this.setState({
               fastForward: forwardAmt
             });
-            this.displayPopup({ icon: "forward", text: `${Math.abs(forwardAmt)} sec`, autoDismiss: false });
+            this.displayPopup({
+              icon: "forward",
+              text: `${Math.abs(forwardAmt)} sec`,
+              autoDismiss: false
+            });
           }}
           onRight={() => {
             const forwardAmt = this.state.fastForward
@@ -155,7 +236,10 @@ export default class Player extends Component<Props, State> {
                 forwardAmt + this.state.video.progress.currentTime
               );
             this.setState({ fastForward: 0 });
-            this.displayPopup({ icon: "forward", text: `${Math.abs(forwardAmt)} sec` });
+            this.displayPopup({
+              icon: "forward",
+              text: `${Math.abs(forwardAmt)} sec`
+            });
           }}
         />
       </View>
@@ -184,15 +268,16 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    paddingLeft: 10,
-    paddingRight: 10,
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
     opacity: 0,
     backgroundColor: "rgba(0, 0, 0, 0.5)",
     height: 60
+  },
+  statusTextContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
   },
   statusText: {
     color: "#FFF",
